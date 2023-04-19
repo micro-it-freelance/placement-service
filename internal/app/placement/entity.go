6bb668b4cab29e0b473891ff522d78c399aa8906
iivkis <razmolodinivan@mail.ru>
2023-04-19 21:20:32 +0700

    review

--- internal/app/placement/entity.go
+++ internal/app/placement/entity.go
@@ -7,13 +7,11 @@ import (
 )
 
 type Placement struct {
-	ID int64 `db:"id" json:"db"`
-
-	TelegramID int64     `db:"telegram_id" json:"telegram_id"`
-	Username   string    `db:"username" json:"username"`
-	CreatedAt  time.Time `db:"created_at" json:"created_at"`
-
-	Typ     placement_service.PlacementType `db:"typ" json:"typ"`
-	Title   string                          `db:"title" json:"title"`
-	Content string                          `db:"content" json:"content"`
+	ID         int64                           `db:"id" json:"db"`
+	TelegramID int64                           `db:"telegram_id" json:"telegram_id"`
+	Username   string                          `db:"username" json:"username"`
+	CreatedAt  time.Time                       `db:"created_at" json:"created_at"`
+	Typ        placement_service.PlacementType `db:"typ" json:"typ"`
+	Title      string                          `db:"title" json:"title"`
+	Content    string                          `db:"content" json:"content"`
 }
